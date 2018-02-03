0560558d7099661f6cd6b15650f17babec437eb5
SantosHM <dosantoshm@gmail.com>
2018-02-03 15:06:08 -0500

    Maquetando el grid de productos más vendidos

--- frontend/views/modules/featured.php
+++ frontend/views/modules/featured.php
@@ -40,6 +40,8 @@
 					</a>
 				</div>
 			</div>
+			<div class="clearfix"></div>
+			<hr>
 		</div>
 		<ul class="grid0">
 			<!-- col-lg-3 no se pone por que el col-md-3 lo reemplaza -->
@@ -49,7 +51,7 @@
 						<img src="http://localhost/ecommerce/backend/views/img/productos/accesorios/accesorio04.jpg" class="img-responsive">
 					</a>	
 				</figure>
-				<h4><small><a href="" class="pixelProducto">Collar de diamantes<br></a></small></h4>
+				<h4><small><a href="" class="pixelProducto">Collar de diamantes<br><br></a></small></h4>
 				<div class="col-xs-6 precio"><h2><small>GRATIS</small></h2></div>
 				<div class="col-xs-6 enlaces">
 					<div class="btn-group pull-right">
@@ -66,7 +68,7 @@
 						<img src="http://localhost/ecommerce/backend/views/img/productos/accesorios/accesorio03.jpg" class="img-responsive">
 					</a>	
 				</figure>
-				<h4><small><a href="" class="pixelProducto">Bolso deportivo gris<br></a></small></h4>
+				<h4><small><a href="" class="pixelProducto">Bolso deportivo gris<br><br></a></small></h4>
 				<div class="col-xs-6 precio"><h2><small>GRATIS</small></h2></div>
 				<div class="col-xs-6 enlaces">
 					<div class="btn-group pull-right">
@@ -83,7 +85,7 @@
 						<img src="http://localhost/ecommerce/backend/views/img/productos/accesorios/accesorio02.jpg" class="img-responsive">
 					</a>	
 				</figure>
-				<h4><small><a href="" class="pixelProducto">Bolso militar<br></a></small></h4>
+				<h4><small><a href="" class="pixelProducto">Bolso militar<br><br></a></small></h4>
 				<div class="col-xs-6 precio"><h2><small>GRATIS</small></h2></div>
 				<div class="col-xs-6 enlaces">
 					<div class="btn-group pull-right">
@@ -100,8 +102,140 @@
 						<img src="http://localhost/ecommerce/backend/views/img/productos/accesorios/accesorio01.jpg" class="img-responsive">
 					</a>	
 				</figure>
-				<h4><small><a href="" class="pixelProducto">Pulsera de diamantes<br></a></small></h4>
+				<h4><small><a href="" class="pixelProducto">Pulsera de diamantes<br>
+					<span class="label label-warning fontSize">Nuevo</span>
+				</a></small></h4>
 				<div class="col-xs-6 precio"><h2><small>GRATIS</small></h2></div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+<div class="container-fluid well well-sm barraProductos">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 organizarProductos">
+				<div class="btn-group pull-right">
+					<button type="button" class="btn btn-default btnGrid" id="btnGrid1">
+						<!-- col-xs-0 se ocultarar en los dispositivos pequeños -->
+						<i class="fa fa-th" aria-hidden="true"></i><span class="col-xs-0"> GRID</span>
+					</button>
+					<button type="button" class="btn btn-default btnList" id="btnList1">
+						<!-- col-xs-0 se ocultarar en los dispositivos pequeños -->
+						<i class="fa fa-list" aria-hidden="true"></i><span class="col-xs-0"> LIST</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid productos">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 tituloDestacado">
+				<!-- con solo poner col-sm-6 ya toma las demás dispositivos con col-6 -->
+				<div class="col-sm-6 col-xs-12">
+					<h1><small>LO MÁS VENDIDO</small></h1>
+				</div>
+				<div class="col-sm-6 col-xs-12">
+					<a href="lo-mas-vendido">
+						<button class="btn btn-default backColor pull-right">
+							VER MÁS <span class="fa fa-chevron-right"></span>
+						</button>
+					</a>
+				</div>
+			</div>
+			<!-- me permite visualizar el hr formateando el float left de las columnas -->
+			<div class="clearfix"></div>
+			<hr>
+		</div>
+		<ul class="grid1">
+			<!-- col-lg-3 no se pone por que el col-md-3 lo reemplaza -->
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/ropa/ropa03.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Falda de flores<br>
+					<span class="label label-warning fontSize">Nuevo</span>
+					<span class="label label-warning fontSize">40% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $29</strong></small>
+					<small>$11</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/ropa/ropa04.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Vestido jean<br>
+					<span class="label label-warning fontSize">40% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $29</strong></small>
+					<small>$11</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/ropa/ropa02.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Vestido clásico<br>
+					<span class="label label-warning fontSize">40% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $29</strong></small>
+					<small>$11</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/ropa/ropa06.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Top dama<br><br>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small>USD $29</small></h2>
+				</div>
 				<div class="col-xs-6 enlaces">
 					<div class="btn-group pull-right">
 						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
