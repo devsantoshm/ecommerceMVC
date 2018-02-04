9b6656cb4d079d0b0e64a425fbfc8bb0ce89d8ea
SantosHM <dosantoshm@gmail.com>
2018-02-04 15:16:00 -0500

    Maquetando Grid de productos mas vistos

--- frontend/views/modules/featured.php
+++ frontend/views/modules/featured.php
@@ -248,3 +248,146 @@
 		</ul>
 	</div>
 </div>
+<div class="container-fluid well well-sm barraProductos">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 organizarProductos">
+				<div class="btn-group pull-right">
+					<button type="button" class="btn btn-default btnGrid" id="btnGrid2">
+						<!-- col-xs-0 se ocultarar en los dispositivos pequeños -->
+						<i class="fa fa-th" aria-hidden="true"></i><span class="col-xs-0"> GRID</span>
+					</button>
+					<button type="button" class="btn btn-default btnList" id="btnList2">
+						<!-- col-xs-0 se ocultarar en los dispositivos pequeños -->
+						<i class="fa fa-list" aria-hidden="true"></i><span class="col-xs-0"> LIST</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container-fluid productos">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 tituloDestacado">
+				<!-- con solo poner col-sm-6 ya toma las demás dispositivos con col-6 -->
+				<div class="col-sm-6 col-xs-12">
+					<h1><small>LO MÁS VISTO</small></h1>
+				</div>
+				<div class="col-sm-6 col-xs-12">
+					<a href="lo-mas-visto">
+						<button class="btn btn-default backColor pull-right">
+							VER MÁS <span class="fa fa-chevron-right"></span>
+						</button>
+					</a>
+				</div>
+			</div>
+			<!-- me permite visualizar el hr formateando el float left de las columnas -->
+			<div class="clearfix"></div>
+			<hr>
+		</div>
+		<ul class="grid1">
+			<!-- col-lg-3 no se pone por que el col-md-3 lo reemplaza -->
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/cursos/curso05.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Curso de bootstrap<br>
+					<span class="label label-warning fontSize">90% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $100</strong></small>
+					<small>$10</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-default btn-xs agregarCarrito" idProducto="444" imagen="http://localhost/ecommerce/backend/views/img/productos/cursos/curso05.jpg" titulo="Curso de Bootstrap" precio="10" tipo="virtual" peso="0" data-toggle="tooltip" title="Agregar al carrito de compras">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+						</button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/cursos/curso04.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Curso de canvas y javascript<br>
+					<span class="label label-warning fontSize">90% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $100</strong></small>
+					<small>$10</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-default btn-xs agregarCarrito" idProducto="444" imagen="http://localhost/ecommerce/backend/views/img/productos/cursos/curso04.jpg" titulo="Curso de Canvas y Javasript" precio="10" tipo="virtual" peso="0" data-toggle="tooltip" title="Agregar al carrito de compras">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+						</button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/cursos/curso02.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Aprende Javascript desde cero<br>
+					<span class="label label-warning fontSize">90% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $100</strong></small>
+					<small>$10</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-default btn-xs agregarCarrito" idProducto="444" imagen="http://localhost/ecommerce/backend/views/img/productos/cursos/curso02.jpg" titulo="Aprende Javascript desde cero" precio="10" tipo="virtual" peso="0" data-toggle="tooltip" title="Agregar al carrito de compras">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+						</button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+			<li class="col-md-3 col-sm-6 col-xs-12">
+				<figure>
+					<a href="#" class="pixelProducto">
+						<img src="http://localhost/ecommerce/backend/views/img/productos/cursos/curso03.jpg" class="img-responsive">
+					</a>	
+				</figure>
+				<h4><small><a href="" class="pixelProducto">Curso de JQuery<br>
+					<span class="label label-warning fontSize">90% off</span>
+				</a></small></h4>
+				<div class="col-xs-6 precio"><h2>
+					<small><strong class="oferta">USD $100</strong></small>
+					<small>$10</small></h2>
+				</div>
+				<div class="col-xs-6 enlaces">
+					<div class="btn-group pull-right">
+						<button type="button" class="btn btn-default btn-xs deseos" idProductos="34" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
+						<button type="button" class="btn btn-default btn-xs agregarCarrito" idProducto="444" imagen="http://localhost/ecommerce/backend/views/img/productos/cursos/curso03.jpg" titulo="Curso de JQuery" precio="10" tipo="virtual" peso="0" data-toggle="tooltip" title="Agregar al carrito de compras">
+							<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+						</button>
+						<a href="" class="pixelProducto">
+							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
+						</a>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
