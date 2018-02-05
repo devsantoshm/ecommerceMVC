3156f7a5c4a0982909fdba8de36ec3c9c6a6f674
SantosHM <dosantoshm@gmail.com>
2018-02-05 13:06:19 -0500

    Agregando jquery a los botones grid y list

--- frontend/views/js/template.js
+++ frontend/views/js/template.js
@@ -13,3 +13,27 @@ $.ajax({
 											"color": textoSuperior})
 	}
 });
+
+var btnList = $(".btnList");
+//console.log("btnList", btnList.length) , numero de clases btnList
+for (var i = 0; i < btnList.length; i++) {
+	$("#btnGrid"+i).click(function(){
+		//extracts parts of a string To extract characters from the end of the string, use a negative start number
+		var numero = $(this).attr("id").substr(-1);
+		$(".list"+numero).hide(); //display:none
+		$(".grid"+numero).show(); //display:block
+
+		$("#btnGrid"+numero).addClass("backColor");
+		$("#btnList"+numero).removeClass("backColor");
+	})
+
+	$("#btnList"+i).click(function(){
+		//extracts parts of a string To extract characters from the end of the string, use a negative start number
+		var numero = $(this).attr("id").substr(-1);
+		$(".list"+numero).show();
+		$(".grid"+numero).hide(); 
+
+		$("#btnGrid"+numero).removeClass("backColor");
+		$("#btnList"+numero).addClass("backColor");
+	})
+}
