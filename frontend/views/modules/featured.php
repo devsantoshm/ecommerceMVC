d5c862387fde179d52e48f7c5e3d84d6145b1e7c
SantosHM <dosantoshm@gmail.com>
2018-02-08 13:21:03 -0500

    Vista grid de productos con datos dinámicos

--- frontend/views/modules/featured.php
+++ frontend/views/modules/featured.php
@@ -1,3 +1,4 @@
+<?php $urlBack = Route::routeServer(); ?>
 <figure class="banner">
 	<img src="http://localhost/ecommerce/backend/views/img/banner/default.jpg" class="img-responsive" width="100%">
 	<div class="textoBanner textoDer">
@@ -77,16 +78,54 @@
 			<?php foreach ($modulos[$i] as $key => $value) { ?>
 			<li class="col-md-3 col-sm-6 col-xs-12">
 				<figure>
-					<a href="#" class="pixelProducto">
-						<img src="http://localhost/ecommerce/backend/<?php echo $value['portada'] ?>" class="img-responsive">
+					<a href="<?php echo $value['ruta'] ?>" class="pixelProducto">
+						<img src="<?php echo $urlBack.$value['portada']; ?>" class="img-responsive">
 					</a>	
 				</figure>
-				<h4><small><a href="" class="pixelProducto"><?php echo $value['titulo'] ?><br><br></a></small></h4>
-				<div class="col-xs-6 precio"><h2><small><?php echo $value['precio'] ?></small></h2></div>
+				<h4><small>
+					<a href="<?php echo $value['ruta'] ?>" class="pixelProducto"><?php echo $value['titulo'] ?>
+						<br><span style="color: rgba(0,0,0,0);">-</span>
+						<?php 
+							if($value['nuevo'] != 0)
+								echo '<span class="label label-warning fontSize">Nuevo</span> ';
+							if($value['oferta'] != 0)
+							  	echo '<span class="label label-warning fontSize">'.$value["descuentoOferta"].'% off</span>';
+						?>
+					</a>
+				</small></h4>
+				<div class="col-xs-6 precio">
+					<?php 
+						if($value['precio'] == 0)
+							echo '<h2><small>GRATIS</small></h2>';
+						else{
+							if($value['oferta'] != 0){
+								echo '<h2>
+										<small><strong class="oferta">USD $'.$value["precio"].'</strong></small>
+										<small>$'.$value["precioOferta"].'</small>
+									 </h2>';
+							}else{
+								echo '<h2><small>USD $'.$value['precio'].'</small></h2>';
+							}
+						}
+					?>
+				</div>
 				<div class="col-xs-6 enlaces">
 					<div class="btn-group pull-right">
 						<button type="button" class="btn btn-default btn-xs deseos" idProductos="<?php echo $value['id'] ?>" data-toggle="tooltip" title="Agregar a mi lista de deseos"><i class="fa fa-heart" aria-hidden="true"></i></button>
-						<a href="" class="pixelProducto">
+						<?php 
+							if ($value['tipo'] == 'virtual') {
+								if ($value['oferta'] != 0) {
+									echo '<button type="button" class="btn btn-default btn-xs agregarCarrito" idProducto="'.$value["id"].'" imagen="'.$urlBack.$value["portada"].'" titulo="'.$value["titulo"].'" precio="'.$value["precioOferta"].'" tipo="'.$value["tipo"].'" peso="'.$value["peso"].'" data-toggle="tooltip" title="Agregar al carrito de compras">
+											<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										</button>';
+								} else {
+									echo '<button type="button" class="btn btn-default btn-xs agregarCarrito" idProducto="'.$value["id"].'" imagen="'.$urlBack.$value["portada"].'" titulo="'.$value["titulo"].'" precio="'.$value["precio"].'" tipo="'.$value["tipo"].'" peso="'.$value["peso"].'" data-toggle="tooltip" title="Agregar al carrito de compras">
+											<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+										</button>';
+								}
+							}	
+						?>
+						<a href="<?php echo $value['ruta'] ?>" class="pixelProducto">
 							<button type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="Ver producto"><i class="fa fa-eye" aria-hidden="true"></i></button>	
 						</a>
 					</div>
