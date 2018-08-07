d3d27a2ffe31af19a3da3492a0f7e358c40b03a7
SantosHM <dosantoshm@gmail.com>
2018-08-07 17:00:40 -0500

    Datos dinámicos productos recientes

--- backend/views/modules/inicio/productos-recientes.php
+++ backend/views/modules/inicio/productos-recientes.php
@@ -1,3 +1,8 @@
+<?php 
+
+$productos = ProductsController::showProductsTotal("fecha");
+
+?>
 <div class="box box-primary">
   <div class="box-header with-border">
     <h3 class="box-title">Productos agregados recientemente</h3>
@@ -10,58 +15,24 @@
   <!-- /.box-header -->
   <div class="box-body">
     <ul class="products-list product-list-in-box">
-      <li class="item">
-        <div class="product-img">
-          <img src="views/dist/img/default-50x50.gif" alt="Product Image">
-        </div>
-        <div class="product-info">
-          <a href="javascript:void(0)" class="product-title">Samsung TV
-            <span class="label label-warning pull-right">$1800</span></a>
-          <span class="product-description">
-                Samsung 32" 1080p 60Hz LED Smart HDTV.
-              </span>
-        </div>
-      </li>
-      <!-- /.item -->
-      <li class="item">
-        <div class="product-img">
-          <img src="views/dist/img/default-50x50.gif" alt="Product Image">
-        </div>
-        <div class="product-info">
-          <a href="javascript:void(0)" class="product-title">Bicycle
-            <span class="label label-info pull-right">$700</span></a>
-          <span class="product-description">
-                26" Mongoose Dolomite Men's 7-speed, Navy Blue.
-              </span>
-        </div>
-      </li>
-      <!-- /.item -->
-      <li class="item">
-        <div class="product-img">
-          <img src="views/dist/img/default-50x50.gif" alt="Product Image">
-        </div>
-        <div class="product-info">
-          <a href="javascript:void(0)" class="product-title">Xbox One <span
-              class="label label-danger pull-right">$350</span></a>
-          <span class="product-description">
-                Xbox One Console Bundle with Halo Master Chief Collection.
-              </span>
-        </div>
-      </li>
-      <!-- /.item -->
-      <li class="item">
-        <div class="product-img">
-          <img src="views/dist/img/default-50x50.gif" alt="Product Image">
-        </div>
-        <div class="product-info">
-          <a href="javascript:void(0)" class="product-title">PlayStation 4
-            <span class="label label-success pull-right">$399</span></a>
-          <span class="product-description">
-                PlayStation 4 500GB Console (PS4)
-              </span>
-        </div>
-      </li>
-      <!-- /.item -->
+      <?php  
+      for($i = 0; $i < 5; $i++){
+        echo '<li class="item">
+                <div class="product-img">
+                  <img src="'.$productos[$i]["portada"].'" alt="Product Image">
+                </div>
+                <div class="product-info">
+                  <a href="javascript:void(0)" class="product-title">'.$productos[$i]["titulo"];
+                  if ($productos[$i]["precio"] == 0) {
+                    echo '<span class="label label-warning pull-right">GRATIS</span></a>';
+                  } else {
+                    echo '<span class="label label-warning pull-right">'.$productos[$i]["precio"].'</span></a>';
+                  }
+              echo '</div>
+              </li>';
+      }
+
+      ?>
     </ul>
   </div>
   <!-- /.box-body -->
